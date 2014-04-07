1bc8a24a175f29383873712705c11130098768d5
Todd Persen <todd.persen@gmail.com>
2014-04-07 16:14:21 -0400

    Build gzip files.

--- config.rb
+++ config.rb
@@ -54,11 +54,9 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
   activate :minify_css
-
-  # Minify Javascript on build
   activate :minify_javascript
+  activate :gzip
 
   # Enable cache buster
   # activate :asset_hash
